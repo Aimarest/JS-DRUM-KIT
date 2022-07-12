82631624764ff4a77eb660ee7d8e299f01558231
Ana <amarcosestevez@gmail.com>
2022-07-12 14:07:29 +0200

    handle function in progress

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 'use strict';
 const keys = document.querySelectorAll('.key');
+console.log(keys);
 
 // Así conseguimos la keyCode de cada letra:
 /*
 window.addEventListener('keydown', (event)=>{
     console.log(event)
 });*/
-function handlePlay (){
-    console.log('hola maricarmen')
 
+//Funciones manejadoras:
+
+function handlePlay (event){
+    console.log(event.keyCode)
+const audio = document.querySelector(`audio[data-key = "${event.keyCode}"]`);
+const key = document.querySelector(`div[data-key = "${event.keyCode}"]`);
+console.log(audio, key)
 };
 
 //Le añadimos al teclado el evento:
 
 window.addEventListener('keydown', handlePlay);
-//Guardamos en una constante cada tecla del array de teclas y añadimos el evento a cada tecla:
+//Le añadimos el evento a cada tecla:
 
-const key = keys.forEach(key => key.addEventListener('click',handlePlay  ))
+//keys.forEach(key => key.addEventListener('click',handlePlay  ));
 
 
 
