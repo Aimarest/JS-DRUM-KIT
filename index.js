2e6952df5afb8c111e4fbce61b815bfa1f799c86
Ana <amarcosestevez@gmail.com>
2022-07-12 13:39:17 +0200

    add events

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 'use strict';
 const keys = document.querySelectorAll('.key');
-// Así conseguimos la keyCode de cada letra:
 
+// Así conseguimos la keyCode de cada letra:
+/*
 window.addEventListener('keydown', (event)=>{
     console.log(event)
-});
+});*/
+function handlePlay (){
+    console.log('hola maricarmen')
 
+};
+
+//Le añadimos al teclado el evento:
+
+window.addEventListener('keydown', handlePlay);
 //Guardamos en una constante cada tecla del array de teclas y añadimos el evento a cada tecla:
 
-const key = keys.forEach(key => key.addEventListener('keydown',handlePlay ))
+const key = keys.forEach(key => key.addEventListener('click',handlePlay  ))
 
-function handlePlay (e){
-console.log(e)
-};
+
 
